4ac169eeb3d36776f59ae44d31a0f948f4e309cc
iMaverick007 <alloydfernandes007@gmail.com>
2025-01-24 16:14:02 +0530

    changes

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../Other/Header';
-import TaskListNumbers from '../other/TaskListNumbers';
+import TaskListNumbers from '../Other/TaskListNumbers';
 import TaskList from '../TaskList/TaskList';
 
 const EmployeeDashboard = ({ changeUser, data }) => {
